e925286aca0d86cedd3dc8f5294602f37aa8573f
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2021-03-16 16:51:04 -0300

    build(dist): automatic new publish

--- dist/index.js
+++ dist/index.js
@@ -1,1 +1,5 @@
-export { default } from "./ld+json";
+import * as f from 'f';
+import hook from 'hook';
+import graph from "./graph";
+import render from "./render";
+export default (structured) => hook(f.once((target) => render(f.push(graph, structured(target)))));
--- dist/ld+json.js
+++
@@ -1,5 +0,0 @@
-import * as f from 'f';
-import hook from 'hook';
-import graph from "./graph";
-import render from "./render";
-export default (target) => hook(f.once((page) => render(f.push(graph, target(page)))));
